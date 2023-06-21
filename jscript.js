de1e99b5cfd46a93f29a799c27245639617a2718
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-21 14:37:36 -0500

    moon position fixed

--- jscript.js
+++ jscript.js
@@ -4,9 +4,11 @@ let background = document.querySelector('.background');
 let stars = document.querySelector('.stars');
 let cloud = document.querySelector('.cloud');
 let sphereBackground = document.querySelector('.sphereBackground');
+let moon = document.querySelector('.moon');
 sphere.addEventListener('click',()=>{
     background.classList.toggle("dusk");
     stars.classList.toggle("newstar");
     cloud.classList.toggle("down");
     sphereBackground.classList.toggle("right");
+    moon.classList.toggle("center");
 });
