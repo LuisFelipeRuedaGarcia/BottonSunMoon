c0b5285cdbbd9b56f91d7805ebe0256a92c51a8f
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-21 13:49:10 -0500

    and the stars come...

--- jscript.js
+++ jscript.js
@@ -1,7 +1,9 @@
 const sphere = document.querySelector('.sphere');
 
 let background = document.querySelector('.background');
+let stars = document.querySelector('.stars');
 
 sphere.addEventListener('click',()=>{
     background.classList.toggle("dusk");
+    stars.classList.toggle("newstar");
 });
