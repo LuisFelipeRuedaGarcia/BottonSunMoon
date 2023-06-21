2cfac63bad31b10222171c74cc3ce377a26919cf
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-21 14:07:46 -0500

    error the clouds don't go away

--- jscript.js
+++ jscript.js
@@ -2,8 +2,9 @@ const sphere = document.querySelector('.sphere');
 
 let background = document.querySelector('.background');
 let stars = document.querySelector('.stars');
-
+let cloud = document.querySelector('.cloud');
 sphere.addEventListener('click',()=>{
     background.classList.toggle("dusk");
     stars.classList.toggle("newstar");
+    cloud.classList.toggle("down");
 });
