23e6a956a1369d5fcfd377efbe59f42fcac9ea53
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-21 14:29:08 -0500

    error sphere dont switch

--- jscript.js
+++ jscript.js
@@ -3,8 +3,10 @@ const sphere = document.querySelector('.sphere');
 let background = document.querySelector('.background');
 let stars = document.querySelector('.stars');
 let cloud = document.querySelector('.cloud');
+let sphereBackground = document.querySelector('.sphereBackground');
 sphere.addEventListener('click',()=>{
     background.classList.toggle("dusk");
     stars.classList.toggle("newstar");
     cloud.classList.toggle("down");
+    sphereBackground.classList.toggle("right");
 });
